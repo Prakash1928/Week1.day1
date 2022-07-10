7e7b7e13ba41324da5ef9c4ba6591946d91a8041
Prakash1928 <109016926+Prakash1928@users.noreply.github.com>
2022-07-10 12:39:32 +0530

    Add files via upload

---
+++ Mobile.java
@@ -0,0 +1,15 @@
+package week1.day1;
+
+public class Mobile {
+	
+	//Method
+	public void makeCall() {	
+	}
+	public void sendMsg() {
+	}
+	
+	//Main method
+	public static void main(String[] args) {
+		System.out.println("This is my mobile");
+	}	
+}
